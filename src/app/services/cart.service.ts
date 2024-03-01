fe97a5f28f91eb0e92e97a5cf9addc9d64d43831
Alejandra Ramirez-Loyer <alejandra.ramirez-loyer@fms-ea.com>
2024-03-01 14:56:27 +0100

    Modif CartService

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -5,23 +5,38 @@ import { Training } from 'src/app/model/training';
   providedIn: 'root'
 })
 export class CartService {
-
+ 
   constructor() { }
+
+ 
 
   listTrainings = [
     {id:1, name: 'Java', description:'Formation Java SE 8 sur 5 jours',price: 1500, quantity:1 }, 
     {id:2, name: 'dotNet', description:'Formation DotNet 3 jours',price: 1000, quantity:1 },
-    {id:1, name: 'Python', description:'Formation Python/Django 5 jours',price:1500, quantity:1 }
+    {id:3, name: 'Python', description:'Formation Python/Django 5 jours',price:1500, quantity:1 }
   ];
 
   listCart : Training[] = []
+
   
+  //Méthode pour ajouter des nouvelles formations dans la liste du panier
   addTraining(training: Training) {
   this.listCart.push(training)
+      }
 
-  }
+
+removeFromCart(training : Training) {
+  this.listCart.splice(this.listCart.indexOf(training), 1)
+}
+
+}
 
   
 
- 
-}
+  
+
+
+
+
+
+
